870c48c1117275d5aae7a1c7917b9284d4383195
Paul Davies <pauldavies@monzo.com>
2020-06-22 20:23:01 +0100

    initial commit with dagger-hilt wiring ready to go

---
+++ app/src/main/java/dev/pauldavies/popularmovies2020/DependencyInjectedColours.kt
@@ -0,0 +1,7 @@
+package dev.pauldavies.popularmovies2020
+
+import javax.inject.Inject
+
+class DependencyInjectedColours @Inject constructor() {
+    val primary: Int = R.color.colorPrimary
+}
---
+++ app/src/main/java/dev/pauldavies/popularmovies2020/PopularMovies2020Application.kt
@@ -0,0 +1,7 @@
+package dev.pauldavies.popularmovies2020
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PopularMovies2020Application : Application()
